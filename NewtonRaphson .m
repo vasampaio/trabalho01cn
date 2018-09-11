4f45869e29e25a80e321ca2e3d1ad1000a90f73b
Hianuy <30296650+Hianuy@users.noreply.github.com>
2018-09-11 19:39:34 -0300

    NewtonRaphson

---
+++ NewtonRaphson .m
@@ -0,0 +1,15 @@
+function [x] = NewtonRaphson(E,a,n,m) %Calculo da raiz via NewtonRaphson, recebe a precisão o a e o intervalo
+  x = (n+m)/2; % o x0 a ser testado é o ponto medio do intervalo
+  f = F(x,a); % cal do f(x0)
+  h=1;
+  printf("\n-----------------------------------------------------------------------------------------\n");
+  printf("h\txn-1\tf(xn-1)\t\tdf(xn-1)\tx\tf(x)\n");
+  while(abs(f)>E)
+    y = x - (F(x,a)/dF(x,a));
+    f = F(y,a);
+    printf("\n%d\t%d\t%d\t%d\t%d\t%d",h,x,F(x,a),dF(x,a),y,F(y,a)); % Printa em "forma de tabela"
+    h++;
+    x=y;
+  endwhile
+  printf("\n-----------------------------------------------------------------------------------------\n");
+endfunction
---
+++ NewtonRaphsonMod .m
@@ -0,0 +1,16 @@
+function [x] = NewtonRaphsonMod(E,a,n,m)% similar ao NewtonRaphson.m recebe as mesmas coisas
+  x = (n+m)/2;
+  f = F(x,a);
+  df = dF(x,a);% calcula a derivada no ponto x0
+  h=1;
+  printf("\n-----------------------------------------------------------------------------------------\n");
+  printf("h\txn-1\tf(xn-1)\t\tdf(x0)\t\tx\tf(x)\n");
+  while((abs(f)>E)&&(h<10000))
+    y = x - (F(x,a)/df); % a derivada no ponto x0 será utilizada em todas as iterações
+    f = F(y,a);
+    printf("\n%d\t%d\t%d\t%d\t%d\t%d",h,x,F(x,a),df,y,f);
+    x=y;
+    h++;
+  endwhile
+  printf("\n-----------------------------------------------------------------------------------------\n");
+endfunction
