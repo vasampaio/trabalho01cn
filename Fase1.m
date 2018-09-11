65c30c9d6dcdc1a7be75b4b35a7d11d9489e6b90
Camila <barbosacamila97@gmail.com>
2018-09-11 18:25:55 -0300

    Add files via upload
    (￣▽￣)ノ

--- Fase1.m
+++ Fase1.m
@@ -1,10 +1,10 @@
-function [n,m] = Fase1(a)
-  x = F(0.00001,a);
-  for i=1:100000
+function [n,m] = Fase1(a) % calcula o isolamento
+  x = F(0.00001,a);  % calcula o f(10^-5)
+  for i=1:100000 % vai de 1 até 100000 testando se há mudança de sinal
     if (x*F(i,a)<0)
-      m = i;
+      m = i; % se hover mudança de sinal o i vira o limite superior
       break;
     endif
   endfor
-  n = m-1+0.00001;
+  n = m-1+0.00001; % e o i-1 o limite inferior
 endfunction
