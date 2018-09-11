b4b5773aa3f7899ccc2a91fa099ec9980d0bcba6
Camila <barbosacamila97@gmail.com>
2018-09-11 18:32:05 -0300

    Update Amax.m
    (・´з`・)

--- Amax.m
+++ Amax.m
@@ -1,14 +1,14 @@
 function [x] = Amax() % calcula o a maximo
-  E = 0.00001; % precis„o de 10^-5
+  E = 0.00001; % precisao de 10^-5
   n = 1; % intervalo entre 1 e 2 observado de forma analitica
   m = 2;
   I = m-n;
   h = 1;
   while((I>E)&&(h<10000))
-    x = (m+n)/2; % usa o metodo da bisseÁ„o para calcular o candidato a raiz
-    [k y] = F1(x); % calcula o isolamento da funÁ„o derivada para aquele candidato a raiz
-    max = F(PFD(0.00001,x,k,y),x); % calcula o valor maximo da funÁ„o para aquele canditado
-    if((max<(2))&&(max>(2-E))) % e testa se ele se aproxima o suficiente de 2 para ser considerado o valor maximo em que n„o h· explos„o
+    x = (m+n)/2; % usa o metodo da bissecao para calcular o candidato a raiz
+    [k y] = F1(x); % calcula o isolamento da funcao derivada para aquele candidato a raiz
+    max = F(PFD(0.00001,x,k,y),x); % calcula o valor maximo da funcao para aquele canditado
+    if((max<(2))&&(max>(2-E))) % e testa se ele se aproxima o suficiente de 2 para ser considerado o valor maximo em que n√£o ha explosao
       h;
       return
     endif
