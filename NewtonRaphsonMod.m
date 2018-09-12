eea62e6061344645cbca4919baceb864c01f3f5f
Camila <barbosacamila97@gmail.com>
2018-09-12 16:01:16 -0300

    Update NewtonRaphsonMod.m
    ༶ඬ༝ඬ༶

--- NewtonRaphsonMod.m
+++ NewtonRaphsonMod.m
@@ -6,7 +6,7 @@ function [x] = NewtonRaphsonMod(E,a,n,m)% similar ao NewtonRaphson.m recebe as m
   printf("\n-----------------------------------------------------------------------------------------\n");
   printf("h\txn-1\tf(xn-1)\t\tdf(x0)\t\tx\tf(x)\n");
   while((abs(f)>E)&&(h<10000))
-    y = x - (F(x,a)/df); % a derivada no ponto x0 será utilizada em todas as iterações
+    y = x - (F(x,a)/df); % a derivada no ponto x0 sera utilizada em todas as iteracoes
     f = F(y,a);
     printf("\n%d\t%d\t%d\t%d\t%d\t%d",h,x,F(x,a),df,y,f);
     x=y;
