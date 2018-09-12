4538116b3f6f2dbbed48100dc5400d689338460e
Camila <barbosacamila97@gmail.com>
2018-09-12 16:03:14 -0300

    Update PosicaoFalsa.m
    (┳◇┳)

--- PosicaoFalsa.m
+++ PosicaoFalsa.m
@@ -1,23 +1,23 @@
-function [x] = PosicaoFalsa(E,a,n,m) % Recebe a precisão a constante e o intervalo, retorna x
+function [x] = PosicaoFalsa(E,a,n,m) % Recebe a precisao a constante e o intervalo, retorna x
   I = m-n; %Calcula o intervalo
-  h=1; % seta a primeira iteração
+  h=1; % seta a primeira iteracao
   printf("\n-----------------------------------------------------------------------------------------\n");
   printf("h\ta\tf(a)\t\tb\tf(b)\t\tx\t\tf(x)\t\tI");
-  while(I>E)% Enquanto o intervalo for maior que a precisão
+  while(I>E)% Enquanto o intervalo for maior que a precisao
     x = ((n*F(m,a))-(m*F(n,a)))/(F(m,a)-F(n,a)); % calculo do x a ser testado
     f = F(x,a);% calculo do F(x) a ser testado
     printf("\n%d\t%d\t%d\t%d\t%d\t%d\t\t%d\t%d\n",h,n,F(n,a),m,F(m,a),x,f,I);
-    if(abs(f)<E)% Testa se o x é valido
+    if(abs(f)<E)% Testa se o x e valido
       printf("\n-----------------------------------------------------------------------------------------\n");
-      return % termina a função
+      return % termina a funcao
     endif
-    if(f*F(n,a)>0) %Determinação dos novos limites do intervalo
+    if(f*F(n,a)>0) %Determinacao dos novos limites do intervalo
       n = x;
     else
       m = x;
     endif
     I = m-n; % calculo do novo intervalo
-    h++; % proxima iteração
+    h++; % proxima iteracao
   endwhile
   printf("\n-----------------------------------------------------------------------------------------\n");
 endfunction
