57eb83e407fd92b095605d4affd6d14388ed6687
Camila <barbosacamila97@gmail.com>
2018-09-12 15:59:46 -0300

    Update F1.m
    (⋟﹏⋞)

--- F1.m
+++ F1.m
@@ -1,8 +1,8 @@
 function [n,m] = F1(a) % calcula o isolamento para a derivada
   x = dF(0.00001,a); % calcula o f(10^-5)
-  for i=1:100000 % vai de 1 até 100000 testando se há mudança de sinal
+  for i=1:100000 % vai de 1 ate 100000 testando se ha mudanca de sinal
     if (x*dF(i,a)<0)
-      m = i; % se hover mudança de sinal o i vira o limite superior
+      m = i; % se hover mudanca de sinal o i vira o limite superior
       break;
     endif
   endfor
